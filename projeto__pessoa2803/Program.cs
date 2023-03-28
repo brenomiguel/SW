bd13a918aaed4d3474ac42e89b6a88960046efa0
Breno Miguel <100931830+brenomiguel@users.noreply.github.com>
2023-03-28 11:06:10 -0300

    Add files via upload

---
+++ projeto__pessoa2803/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace projeto__pessoa2803;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa("Fulano",54);
+        //p1.InsereNome("Breno");
+        //p1.InsereSobreNome("Miguel");
+        //p1.InsereIdade(15);
+        p1.MostraDados();
+    }
+}
