1c1cb658bc968f2e83b0b80241ed971728704b9a
Breno Miguel <100931830+brenomiguel@users.noreply.github.com>
2023-04-24 20:38:06 -0300

    Update Banco.cs

--- Banco_emprestimo/Banco.cs
+++ Banco_emprestimo/Banco.cs
@@ -24,23 +24,24 @@ class Banco
              this.Saldo -= valor;
         }
     }
-    public string soma(double n1, double n2, double n3){
-        this.num1 = n1;
-        this.num2 = n2;
-        this.num3 = n3;
-        double soma1 = this.num1 * this.num2;
-        double soma2 = this.num1 / this.num3;
-        
+    public double parcela {get; set;}
+    public double valor {get; set;}
 
-        double resultado = this
-
-    
+    public string emprestimo(int parcelas, double dinheiro){
+        this.parcela = parcelas;
+        this.valor = dinheiro;
         
+        double div = dinheiro/parcelas;
+        double mult = dinheiro * 0.06;
+        double soma = mult + div;
+        double total = soma *parcelas;
+
+        string resultado = "O valor da sua parcela sem juros é: "+ div +". O valor da parcela com juros: "+ mult + ". O valor do juros com prestações é de: "+ soma +"e o valor total a pagar é de: "+total;
+        return resultado;
+        
+     
     }
-    public double MostrarSaldo(){
-        return this.Saldo + this.Limite;
-    }
-    public void imprime(string texto){
+    public void imprimi(string texto){
         Console.WriteLine(texto);
     }
 
